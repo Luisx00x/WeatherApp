a36c30108c2d779a020b5952f924275e977e3ece
Luis Marcano <lmarcano203@gmail.com>
2023-06-06 21:23:32 -0500

    feature: init Details component

---
+++ src/components/Modals/Details/Details.jsx
@@ -0,0 +1,13 @@
+
+const Details = () => {
+  return (
+    <div className={`modals`}>
+      <div className={`modals-message`}>
+        <h2>Details</h2>
+        <button>Regresar</button>
+      </div>
+    </div>
+  )
+}
+
+export default Details;
