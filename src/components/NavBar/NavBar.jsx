a5bb9f434c2e7c27b8584716d605a3e3295bd7c7
Luis Marcano <lmarcano203@gmail.com>
2023-06-13 22:49:17 -0500

    fix: mobile and desktop navbar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
+
 import s from './NavBar.module.css';
 import SearchCity from "../SearchCity/SearchCity";
+import Image from "next/image";
 
 export default function NavBar(){
   return (
     <nav className={s.container}>
-      <Link href={"/"}>Home</Link>
+      <Image width={75} height={75} src={'/img/logo_bg.png'} className={s.logo}/>
+      <h1 className={s.title}>WeatherApp</h1>
       <div>
         <SearchCity/>
       </div>
