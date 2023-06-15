963ee1658b4ad2e99c3713716cf622260946588d
Luis Marcano <lmarcano203@gmail.com>
2023-06-14 23:56:17 -0500

    fix: adding process.env to variable

--- src/components/SearchCity/SearchCityHandlers.js
+++ src/components/SearchCity/SearchCityHandlers.js
@@ -11,7 +11,7 @@ export function inputHandler(e, set){
 
 export function submitHandler(e, city, apiKey, setNewCity, error){
   e.preventDefault();
-  useFetch(`${NEXT_PUBLIC_API_URL}/weather?q=${city}&appid=${apiKey}&units=metric`,
+  useFetch(`${process.env.NEXT_PUBLIC_API_URL}/weather?q=${city}&appid=${apiKey}&units=metric`,
     (newCity => {
       setNewCity( prev => {
         if(!prev.find(city => city.id === newCity.id)) return [...prev, newCity]
