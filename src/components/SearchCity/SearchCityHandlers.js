ab6b98b9260c2e89295dfb3af7ef779a60e69302
Luis Marcano <lmarcano203@gmail.com>
2023-06-14 23:54:01 -0500

    fixing API_URL: changing "http" to "https"

--- src/components/SearchCity/SearchCityHandlers.js
+++ src/components/SearchCity/SearchCityHandlers.js
@@ -11,7 +11,7 @@ export function inputHandler(e, set){
 
 export function submitHandler(e, city, apiKey, setNewCity, error){
   e.preventDefault();
-  useFetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+  useFetch(`${NEXT_PUBLIC_API_URL}/weather?q=${city}&appid=${apiKey}&units=metric`,
     (newCity => {
       setNewCity( prev => {
         if(!prev.find(city => city.id === newCity.id)) return [...prev, newCity]
